45fa0d5c8fcda992ffb55f604720b14d60bb74ad
Martin Tveter <martin.skjelsvik.tveter@nav.no>
2022-01-26 09:29:51 +0100

    rename til app.jar

--- build.gradle.kts
+++ build.gradle.kts
@@ -41,3 +41,7 @@ tasks.withType<KotlinCompile> {
 tasks.withType<Test> {
 	useJUnitPlatform()
 }
+
+tasks.getByName<org.springframework.boot.gradle.tasks.bundling.BootJar>("bootJar") {
+	this.archiveFileName.set("app.jar")
+}
