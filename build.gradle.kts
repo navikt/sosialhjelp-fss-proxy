207721b0c3178b6977011efa7ab1e2d0fc2d7981
Martin Tveter <martin.skjelsvik.tveter@nav.no>
2022-09-06 11:12:41 +0200

    snyk: bump snakeyaml Også, oppgraderer deps

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,22 +3,24 @@ import org.gradle.api.tasks.testing.logging.TestLogEvent
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 object Versions {
-    const val springboot = "2.7.1"
-    const val coroutines = "1.6.3"
-    const val jackson = "2.13.3"
-    const val micrometer = "1.9.2"
+    const val springboot = "2.7.3"
+    const val coroutines = "1.6.4"
+    const val jackson = "2.13.4"
+    const val micrometer = "1.9.3"
     const val javaJwt = "4.0.0"
     const val jwksRsa = "0.21.1"
     const val logstash = "7.2"
-    const val nimbusJoseJwt = "9.23"
+    const val nimbusJoseJwt = "9.24.3"
+
+    const val snakeyaml = "1.31"
 
     // test
     const val junit = "5.8.2"
-    const val mockk = "1.12.4"
+    const val mockk = "1.12.7"
 }
 
 plugins {
-    id("org.springframework.boot") version "2.7.1"
+    id("org.springframework.boot") version "2.7.3"
     kotlin("jvm") version "1.7.10"
     kotlin("plugin.spring") version "1.7.10"
     id("org.jlleitschuh.gradle.ktlint") version "10.3.0"
@@ -55,6 +57,12 @@ dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter:${Versions.junit}")
     testImplementation("io.mockk:mockk:${Versions.mockk}")
     testImplementation("org.springframework.boot:spring-boot-starter-test:${Versions.springboot}")
+
+    constraints {
+        implementation("org.yaml:snakeyaml:${Versions.snakeyaml}") {
+            because("Snyk ønsker versjon 1.31 eller høyere")
+        }
+    }
 }
 
 tasks.withType<KotlinCompile> {
